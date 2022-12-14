32fc255a789bf0c3168765bd3ace47a88c233a1a
vasunagarajan <120562643+vasunagarajan@users.noreply.github.com>
2022-12-14 16:15:32 +0530

    Update translator.py

--- final_project/translator.py
+++ final_project/translator.py
@@ -4,4 +4,4 @@ language_translator = LanguageTranslatorV3(
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+language_translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/918f28be-0dc4-4344-9f63-dd4053d6d487')
